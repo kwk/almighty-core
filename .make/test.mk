c5bb90d217bc7ea6449c39c0630e12592f6d2018
Konrad Kleine <kkleine@redhat.com>
2016-09-28 12:34:56 +0200

    Make sqlbindata.go a dependency of test-unit and test-integration
    Otherwise the migration/sqlbindata.go file will not exist.

--- .make/test.mk
+++ .make/test.mk
@@ -114,11 +114,11 @@ test-all: prebuild-check test-unit test-integration
 
 .PHONY: test-unit
 ## Runs the unit tests and produces coverage files for each package.
-test-unit: prebuild-check clean-coverage-unit $(COV_PATH_UNIT)
+test-unit: prebuild-check clean-coverage-unit migration/sqlbindata.go $(COV_PATH_UNIT)
 
 .PHONY: test-integration
 ## Runs the integration tests and produces coverage files for each package.
-test-integration: prebuild-check clean-coverage-integration migrate-database $(COV_PATH_INTEGRATION)
+test-integration: prebuild-check clean-coverage-integration migrate-database migration/sqlbindata.go $(COV_PATH_INTEGRATION)
 
 # Downloads docker-compose to tmp/docker-compose if it does not already exist.
 define download-docker-compose
