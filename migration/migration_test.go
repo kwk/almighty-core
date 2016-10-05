fc2af422a433d35941783227be6837ae76e9f716
Baiju Muthukadan <baiju.m.mail@gmail.com>
2016-10-05 14:48:33 +0530

    Use 20 concurrent db connections for testing
    - Use testify
    - Use all DB connection params

--- migration/migration_test.go
+++ migration/migration_test.go
@@ -9,63 +9,40 @@ import (
 	"github.com/almighty/almighty-core/resource"
 	"github.com/jinzhu/gorm"
 	_ "github.com/lib/pq"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConcurrentMigrations(t *testing.T) {
 	resource.Require(t, resource.Database)
-
-	var db1 *gorm.DB
 
 	var err error
 	if err = configuration.Setup(""); err != nil {
 		panic(fmt.Errorf("Failed to setup the configuration: %s", err.Error()))
 	}
 
-	db1, err = gorm.Open("postgres",
-		fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=%s",
-			configuration.GetPostgresHost(),
-			configuration.GetPostgresPort(),
-			configuration.GetPostgresUser(),
-			configuration.GetPostgresPassword(),
-			configuration.GetPostgresSSLMode(),
-		))
-	if err != nil {
-		t.Fatal("Cannot connect to DB", err)
-	}
-	defer db1.Close()
-
-	var db2 *gorm.DB
-
-	db2, err = gorm.Open("postgres",
-		fmt.Sprintf("host=%s port=%d user=%s password=%s sslmode=%s",
-			configuration.GetPostgresHost(),
-			configuration.GetPostgresPort(),
-			configuration.GetPostgresUser(),
-			configuration.GetPostgresPassword(),
-			configuration.GetPostgresSSLMode(),
-		))
-	if err != nil {
-		t.Fatal("Cannot connect to DB", err)
-	}
-	defer db2.Close()
-
 	var wg sync.WaitGroup
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err := Migrate(db1)
+	for i := 0; i < 20; i++ {
+		wg.Add(1)
+		db, err := gorm.Open("postgres",
+			fmt.Sprintf("host=%s port=%d user=%s password=%s DB.name=%s sslmode=%s",
+				configuration.GetPostgresHost(),
+				configuration.GetPostgresPort(),
+				configuration.GetPostgresUser(),
+				configuration.GetPostgresPassword(),
+				configuration.GetPostgresDatabase(),
+				configuration.GetPostgresSSLMode(),
+			))
 		if err != nil {
-			t.Error(err.Error())
+			t.Fatal("Cannot connect to DB", err)
 		}
-	}()
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		err := Migrate(db2)
-		if err != nil {
-			t.Error(err.Error())
-		}
-	}()
+
+		go func(db *gorm.DB) {
+			defer wg.Done()
+			err = Migrate(db)
+			assert.Nil(t, err)
+		}(db)
+
+	}
 	wg.Wait()
 }
