fecb1c9f51b07a54c8be4aa0da5b126f5f3c976c
Konrad Kleine <kkleine@redhat.com>
2017-02-24 13:12:22 +0100

    Fix compile error
    ./iteration.go:290: convertToUUID redeclared in this block
    	previous declaration at ./area.go:262

--- area.go
+++ area.go
@@ -258,13 +258,3 @@ func createAreaLinks(request *goa.RequestData, id interface{}) *app.GenericLinks
 		Self: &selfURL,
 	}
 }
-
-func convertToUUID(uuidStrings []string) []uuid.UUID {
-	var uUIDs []uuid.UUID
-
-	for i := 0; i < len(uuidStrings); i++ {
-		uuidString, _ := uuid.FromString(uuidStrings[i])
-		uUIDs = append(uUIDs, uuidString)
-	}
-	return uUIDs
-}
